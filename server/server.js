1c162e6e113c1d8f0efba01cf08dfe0d3c678d27
Shashank Shekhar <shekhars027@gmail.com>
2025-11-19 23:49:08 +0530

    Add vercel cors

--- server/server.js
+++ server/server.js
@@ -7,7 +7,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (origin.includes('.vercel.app')) return callback(null, true);
+
+    callback(new Error('Not allowed by CORS'));
+  },
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Routes
